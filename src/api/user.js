a59ee9ff627737986861a41f0c2de291c3f1c97f
mangzouzou\wathet <chuchen0126@163.com>
2022-10-24 18:31:39 +0800

    项目配置end

--- src/api/user.js
+++ src/api/user.js
@@ -1,24 +1,7 @@
-import request from '@/utils/request'
+// 用户相关的接口
+import { request } from '@/utils/request.js'
 
-export function login(data) {
-  return request({
-    url: '/vue-admin-template/user/login',
-    method: 'post',
-    data
-  })
-}
-
-export function getInfo(token) {
-  return request({
-    url: '/vue-admin-template/user/info',
-    method: 'get',
-    params: { token }
-  })
-}
-
-export function logout() {
-  return request({
-    url: '/vue-admin-template/user/logout',
-    method: 'post'
-  })
-}
+/**
+ * 实例
+ */
+export const getUser = () => request({})
